e0509ea74b6e778778e542d2302798503a0ec1ff
thomas.caron <tc.caron91@gmail.com>
2023-09-01 10:24:35 +0200

    ➕ Header categories

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -14,6 +14,28 @@ const Header = () => {
         <img className={styles.capy} src="/images/capybara_right.png" alt="capybara right" />
       </div>
       <div className={styles.headerLowerPane}/>
+      <div className={styles.statsContainer}>
+        <div className={styles.stat}>
+          <img className={styles.statIcon} src="/icons/basket.png" alt="heart" />
+          <p className={styles.statTitle}>For Sale</p>
+          <p className={styles.statValue}>+500</p>
+        </div>
+        <div className={styles.stat}>
+          <img className={styles.statIcon} src="/icons/capybaras.png" alt="heart" />
+          <p className={styles.statTitle}>Capybaras</p>
+          <p className={styles.statValue}>5,200</p>
+        </div>
+        <div className={styles.stat}>
+          <img className={styles.statIcon} src="/icons/articles.png" alt="heart" />
+          <p className={styles.statTitle}>Articles</p>
+          <p className={styles.statValue}>+1,820</p>
+        </div>
+        <div className={styles.stat}>
+          <img className={styles.statIcon} src="/icons/community.png" alt="heart" />
+          <p className={styles.statTitle}>Members</p>
+          <p className={styles.statValue}>500,000</p>
+        </div>
+      </div>
     </header>
   )
 }
